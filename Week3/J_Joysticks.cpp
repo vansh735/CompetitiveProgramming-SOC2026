8b740029bbde2844b5c31ad53cc8e00d262b075f
vansh735 <goelvansh2213@gmail.com>
2026-06-25 22:46:15 +0530

    Create J_Joysticks.cpp

---
+++ Week3/J_Joysticks.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a1, a2;
+    cin >> a1 >> a2;
+    
+    int count = 0;
+    while (a1 > 0 && a2 > 0) {
+        if (a1 == 1 && a2 == 1) {
+            break;
+        }
+        
+        if (a1 < a2) {
+            a1 += 1;
+            a2 -= 2;
+        } else {
+            a2 += 1;
+            a1 -= 2;
+        }
+        count++;
+    }
+    
+    cout << count << "\n";
+    
+    return 0;
+}
